8ab38503d99114ce125362fbfa9e3a67adf0557b
arafat583 <arafat.rahman.fahim.bd12@gmail.com>
2024-10-05 16:23:26 +0600

    tuple operations using python

---
+++ Lists, play with lists and word matching using python/lists.py
@@ -0,0 +1,13 @@
+empty_list = []
+print(empty_list)
+
+number = [1, 2, 3, 4, 5]
+print(number)
+
+triples = [1, 2, 3] * 3
+print(triples)
+
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"\n")
+
---
+++ tuple operations using python/main.py
@@ -0,0 +1,17 @@
+tuplex = ("Hello", True, 1.1, 5)
+
+print(tuplex)
+print(tuplex[1])
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuplex = ("This text will be replaced with the previous Values")
+print(tuplex)
+
+tuple1 = (50, 50, 50, 50, 30, 20, 10, 9, 8)
+print(tuple1.count(50))
+
+slice = tuple1[3:5]
+print(slice)
+
