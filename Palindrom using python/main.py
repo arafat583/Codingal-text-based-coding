057ddccd4e7c5c31db2ad60768dd3eadb0f10d40
arafat583 <arafat.rahman.fahim.bd12@gmail.com>
2024-10-05 16:33:27 +0600

    flip flop using python

---
+++ Palindrom using python/main.py
@@ -0,0 +1,17 @@
+def palind(r):
+    s = 0
+    e = len(r) - 1
+    while(s>e):
+        if(r[s]!=r[e]):
+            return False
+        s += 1
+        e += 1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if palind(r):
+    print("The tuple is flip flop")
+else:
+    print("The tuple is not Flip flop")    
+
