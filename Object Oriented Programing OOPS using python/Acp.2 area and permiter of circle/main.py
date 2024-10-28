b160dbdfa76300532154ad616218b44586ab244a
arafat583 <arafat.rahman.fahim.bd12@gmail.com>
2024-10-28 21:55:22 +0600

    OOPS area and parameterof circle  using python

---
+++ Object Oriented Programing OOPS using python/Acp.2 area and permiter of circle/main.py
@@ -0,0 +1,11 @@
+import math
+
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * radius ** 2
+perimeter = 2 * math.pi * radius
+
+
+print("The area of the circle:", area)
+print("the perimeter of the circle:", perimeter)
