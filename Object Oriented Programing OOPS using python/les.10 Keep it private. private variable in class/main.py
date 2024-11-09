7774005a17d0b4fbaac1403063a8394bc10cde05
arafat583 <arafat.rahman.fahim.bd12@gmail.com>
2024-11-09 16:36:22 +0600

    Point function using python oops

--- Object Oriented Programing OOPS using python/les.10 Keep it private. private variable in class/main.py
+++ Object Oriented Programing OOPS using python/les.10 Keep it private. private variable in class/main.py
@@ -9,3 +9,30 @@ class Myclass:
 
 foo = Myclass()
 foo.hello()
+
+print("-------------")
+
+class computer:
+
+    def __init__(self):
+        self.__maxprice = 500
+
+
+    def sell(self):
+        print("Selling price: {}".format(self.__maxprice))
+
+     # To change private variable:
+    def setmaxprice(self, price):
+        self.__maxprice = price     
+
+
+c = computer()
+c.sell()
+
+c.__maxprice = 1000
+c.sell()
+
+c.setmaxprice(1000)
+c.sell()
+
+
